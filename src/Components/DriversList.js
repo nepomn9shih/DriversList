2e361e5aa63d8e7084b2df7cab4b947865f76c46
nepomn9shih <nepomn9shih@gmail.com>
2020-10-23 18:23:15 +0300

    Добавлен поиск по имени, фамилии и национальности

--- src/Components/DriversList.js
+++ src/Components/DriversList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Driver} from './Driver'
 import Pagination from 'react-js-pagination'
 
@@ -10,10 +10,31 @@ export const DriversList = props => {
     handlePageChange,
     activePage
   } = props
- 
+
+//start search  
+let [filtered, setFiltered] = useState(driversList)
+
+const onTextChanged = (e) => {
+    let text = e.target.value.trim(); 
+    let filteredList = driversList.filter((driver) => {return (
+        (driver.nationality.toLowerCase().search(text) !== -1) || 
+        (driver.givenName.toLowerCase().search(text) !== -1) ||
+        (driver.familyName.toLowerCase().search(text) !== -1) 
+        )})
+    setFiltered(filteredList)
+}   
+//end search
+
   return (
     <div>
-      {driversList && driversList.map((driver, index) => (
+        <div className="input-group m-2 w-25">
+            <div className="input-group-prepend">
+                <span role="img" className="input-group-text" aria-label="">&#128269;</span>
+                
+            </div>
+            <input type="text" className="form-control" placeholder="Поиск" onChange={onTextChanged} />
+        </div>
+      {filtered && filtered.map((driver, index) => (
         <Driver key={driver.driverId} driver={driver} index={index+1} activePage={activePage}/>
       ))}
       <div className="d-flex justify-content-center align-items-center mt-3 paginationStyle">
